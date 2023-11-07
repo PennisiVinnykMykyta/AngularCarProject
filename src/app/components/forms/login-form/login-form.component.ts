11d0ac67cd7a7d14825820f83d0f3eaeb8d68efa
Mykyta <sayron.arnorius@yahoo.com>
2023-11-07 17:08:43 +0100

    feature: added logOut possibility

---
+++ src/app/components/forms/login-form/login-form.component.ts
@@ -0,0 +1,37 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import {UserDisplayTemplate} from "../../templates/dto-templates/user-display-template";
+import {AuthenticationService} from "../../services/authentication.service";
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent {
+  email?: string;
+  password?: string;
+  message?: string;
+
+  @Output() userData: EventEmitter<UserDisplayTemplate> = new EventEmitter<UserDisplayTemplate>();
+
+
+  constructor(private authService: AuthenticationService) {
+  }
+
+  protected readonly faCheck = faCheck;
+
+  verify() {
+    this.authService.getUser(this.email!,this.password!).subscribe(user => {
+
+      if(user!==null && user !== undefined){
+        this.userData.emit(user);
+
+      }else{
+        this.email = '';
+        this.password = '';
+        this.message = "No such user in the database";
+      }
+    });
+  }
+}
